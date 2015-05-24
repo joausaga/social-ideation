3eec687c0dd48a0b4a9bdee6608f3d6fcbbe8213
joausaga <jorgesaldivar@gmail.com>
2015-05-24 18:31:10 +0200

    Change parameters of celery configuration

--- social_ideation/celery.py
+++ social_ideation/celery.py
@@ -9,14 +9,14 @@ from django.conf import settings
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'social_ideation.settings')
 
-app = Celery('social_ideation')
+ce_app = Celery('social_ideation')
 
 # Using a string here means the worker will not have to
 # pickle the object when using Windows.
-app.config_from_object('django.conf:settings')
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+ce_app.config_from_object('django.conf:settings')
+ce_app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
 
-@app.task(bind=True)
+@ce_app.task(bind=True)
 def debug_task(self):
     print('Request: {0!r}'.format(self.request))
