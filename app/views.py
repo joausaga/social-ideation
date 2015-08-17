e7fc6fe937625a120cb1202e25be073fe4151dbb
joausaga <jorgesaldivar@gmail.com>
2015-08-17 17:56:10 +0200

    Debugging

--- app/views.py
+++ app/views.py
@@ -28,13 +28,11 @@ def fb_real_time_updates(request):
                 return HttpResponse(challenge)
         elif request.method == 'POST':
             logger.info('There is an update!')
-            logger.info(request.META)
-            req_signature = request.META.get('X_HUB_SIGNATURE')
+            req_signature = request.META.get('HTTP_X_HUB_SIGNATURE')
             logger.info(req_signature)
-            exp_signature = hashlib.sha1('sha1='+request.body+fb_app.app_secret)
+            exp_signature = hashlib.sha1('sha1='+request.body+fb_app.app_secret).hexdigest()
             logger.info(exp_signature)
             req_json = json.loads(request.body)
-            logger.info(req_json)
             if req_signature == exp_signature:
                 logger.info(req_json)
                 return HttpResponse()
